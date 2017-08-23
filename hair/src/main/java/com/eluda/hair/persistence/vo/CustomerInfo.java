1f61765561c9632cc8ee3107ea77f8bbd66c4622
gitolite <gitolite@119.205.221.44>
2017-08-23 17:56:38 +0900

    2017.08.23-2

---
+++ hair/src/main/java/com/eluda/hair/persistence/vo/CustomerInfo.java
@@ -0,0 +1,26 @@
+package com.eluda.hair.persistence.vo;
+
+public class CustomerInfo {
+	private String id;
+	private String name;
+	private String phoneNumber;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+}
